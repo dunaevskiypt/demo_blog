9828bcfde0c3fcc422ea74056685891fc4fc8b6b
peter <dunaevskiyp@gmail.com>
2023-11-16 10:20:39 +0200

    error

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-debug-toolbar',
     'django_extensions',
     'app_blog.apps.AppBlogConfig',
 ]
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
